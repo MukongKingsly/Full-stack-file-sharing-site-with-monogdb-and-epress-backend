d0398bd89cfac82bd2f5fa0dfd89411895397c81
Mukong <kingmbamu@yahoo.co.uk>
2023-10-18 14:21:39 +0200

    added try catch blocks to async funtions

--- index.js
+++ index.js
@@ -27,41 +27,49 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload", upload.single("file"), async (req, res) => {
-  const fileData = {
-    path: req.file.path,
-    originalName: req.file.originalname,
-  };
-  if (req.body.password != null && req.body.password !== "") {
-    fileData.password = await bcrypt.hash(req.body.password, 10);
+  try {
+    const fileData = {
+      path: req.file.path,
+      originalName: req.file.originalname,
+    };
+    if (req.body.password != null && req.body.password !== "") {
+      fileData.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const file = await File.create(fileData);
+
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
+  } catch (error) {
+    throw new Error(error);
   }
-
-  const file = await File.create(fileData);
-
-  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
 
 app.route("/file/:id").get(handleDownload).post(handleDownload);
 
 async function handleDownload(req, res) {
-  const file = await File.findById(req.params.id);
+  try {
+    const file = await File.findById(req.params.id);
 
-  if (file.password != null) {
-    if (req.body.password == null) {
-      res.render("password");
-      return;
+    if (file.password != null) {
+      if (req.body.password == null) {
+        res.render("password");
+        return;
+      }
+
+      if (!(await bcrypt.compare(req.body.password, file.password))) {
+        res.render("password", { error: true });
+        return;
+      }
     }
 
-    if (!(await bcrypt.compare(req.body.password, file.password))) {
-      res.render("password", { error: true });
-      return;
-    }
+    file.downloadCount++;
+    await file.save();
+    console.log(file.downloadCount);
+
+    res.download(file.path, file.originalName);
+  } catch (error) {
+    throw new Error(error);
   }
-
-  file.downloadCount++;
-  await file.save();
-  console.log(file.downloadCount);
-
-  res.download(file.path, file.originalName);
 }
 
 app.listen(process.env.PORT, () =>
